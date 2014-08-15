409862ffdcdb7a7558aed779e7e580dee92da74d
bernardop <berna.ipach@gmail.com>
2014-08-15 04:12:26 -0400

    Stickers page done.

--- PaniniWS.Web/app/controllers/stickersController.js
+++ PaniniWS.Web/app/controllers/stickersController.js
@@ -3,9 +3,26 @@ app.controller('stickersController', ['$scope', '$routeParams', 'stickersService
 
     $scope.message = '';
     $scope.stickers = [];
+    $scope.albumPages = [];
+    $scope.currentPage = 1;
+
+    $scope.filterByPageID = function (element) {
+        return element.albumSticker.albumPage.albumPageID == $scope.currentPage ? true : false
+    }
 
     stickersService.getStickers($routeParams.albumID).then(function (results) {
-        $scope.stickers = results.data;
+        var stickers = results.data;
+
+        $scope.stickers = stickers;
+
+        var flags = [], output = [], l = stickers.length, i;
+        for (i = 0; i < l; i++) {
+            if (flags[stickers[i].albumSticker.albumPage.albumPageID]) continue;
+            flags[stickers[i].albumSticker.albumPage.albumPageID] = true;
+            output.push({ pageID: stickers[i].albumSticker.albumPage.albumPageID, description: stickers[i].albumSticker.albumPage.description });
+        }
+
+        $scope.albumPages = output;
     },
     function (err) {
         $scope.message = err.error_description;
